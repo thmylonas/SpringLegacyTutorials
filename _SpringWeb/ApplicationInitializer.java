64d1726795340326fdeb6ec8b012f0836176fc23
thmylonas <thmylonas@hotmail.com>
2025-10-27 13:52:56 +0100

    [Part_3] The "SpringWeb" is moved from "SpringMvcJsfPfWebApp"

---
+++ _SpringWeb/ApplicationInitializer.java
@@ -0,0 +1,24 @@
+package com.thomasmylonas.spring_mvc_jsf_pf_web_app;
+
+import com.thomasmylonas.spring_mvc_jsf_pf_web_app.config.DataAccessConfig;
+import com.thomasmylonas.spring_mvc_jsf_pf_web_app.config.RootConfig;
+import com.thomasmylonas.spring_mvc_jsf_pf_web_app.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class, DataAccessConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
