493c8b4bffa18509a0201f131d63b12a89b7c6da
thmylonas <thmylonas@hotmail.com>
2025-11-16 18:13:06 +0100

    [Part_27] The "SpringRest" is created: Empty "WebConfig" is created

---
+++ SpringRest/src/main/java/com/thomasmylonas/spring_rest/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.thomasmylonas.spring_rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = {"com.thomasmylonas.spring_rest.controllers"},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)
+        }
+)
+@EnableWebMvc
+public class WebConfig /*implements WebMvcConfigurer*/ {
+}
