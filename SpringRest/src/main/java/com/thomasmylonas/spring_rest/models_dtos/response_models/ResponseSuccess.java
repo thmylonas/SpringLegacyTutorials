7a71b3f049b0c92f777c61425a742ee13763fc4b
thmylonas <thmylonas@hotmail.com>
2025-11-26 18:39:14 +0100

    [Part_63] The "SpringRest" is created: Right use of "@JsonSerialize", "@JsonFormat"

--- SpringRest/src/main/java/com/thomasmylonas/spring_rest/models_dtos/response_models/ResponseSuccess.java
+++ SpringRest/src/main/java/com/thomasmylonas/spring_rest/models_dtos/response_models/ResponseSuccess.java
@@ -2,6 +2,8 @@ package com.thomasmylonas.spring_rest.models_dtos.response_models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -10,8 +12,10 @@ import java.util.Map;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ResponseSuccess(
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime timestamp,
+
         String statusCode,
         String message,
         String path, // "request URL"
--- SpringRest/src/main/java/com/thomasmylonas/spring_rest/models_dtos/student_dtos/StudentResponseDto.java
+++ SpringRest/src/main/java/com/thomasmylonas/spring_rest/models_dtos/student_dtos/StudentResponseDto.java
@@ -2,6 +2,8 @@ package com.thomasmylonas.spring_rest.models_dtos.student_dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.thomasmylonas.spring_rest.entities.enums.Status;
 import lombok.Builder;
 
@@ -9,6 +11,7 @@ import java.time.LocalDate;
 
 @Builder
 public record StudentResponseDto(
+        @JsonProperty(value = "id")
         Long id,
 
         @JsonProperty(value = "last_name")
@@ -17,15 +20,18 @@ public record StudentResponseDto(
         @JsonProperty(value = "first_name")
         String firstName,
 
+        @JsonSerialize(using = LocalDateSerializer.class)
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
         @JsonProperty(value = "date_of_birth")
         LocalDate dateOfBirth,
 
+        @JsonProperty(value = "absences")
         Integer absences,
 
         @JsonProperty(value = "department_id")
         String departmentId,
 
+        @JsonProperty(value = "status")
         Status status
 ) {
 }
