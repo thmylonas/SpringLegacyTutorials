1742b57f0837d1aaa548c9ad5d6c0a47a2545aad
thmylonas <thmylonas@hotmail.com>
2026-05-21 11:06:42 +0200

    The enum is updated: Lombok is added

--- SpringRest/src/main/java/com/thomasmylonas/spring_rest/entities/enums/Status.java
+++ SpringRest/src/main/java/com/thomasmylonas/spring_rest/entities/enums/Status.java
@@ -1,5 +1,10 @@
 package com.thomasmylonas.spring_rest.entities.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Status {
 
     ADVANCED_STUDENT("Advanced Student"),
@@ -7,12 +12,4 @@ public enum Status {
     JUNIOR_STUDENT("Junior Student");
 
     private final String value;
-
-    Status(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
 }
